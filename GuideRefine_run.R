5b4942cffcc0fc2b4127fe814803fb858ee9b11d
Stefanus Bernard <stefanus.bernard@gmail.com>
2025-11-06 18:33:59 +0000

    separate T2T-CHM13 annotation script

--- GuideRefine_run.R
+++ GuideRefine_run.R
@@ -2,39 +2,40 @@ library(rmarkdown)
 
 # Define parameters
 my_params <- list(
-  output_filename = "M1_refined",
+  output_filename = "jacquere_refined_report",
   
-  library_dir = "./cellecta_library/",
-  sgrna_library = "sample_M1_150K",
+  library_dir = "./public_crispr_library/",
+  sgrna_library = "Jacquere_PerGuideAnnotations_Quota4",
   terms = c("CONTROL", "Control", "control", "INTRON", "Intron", "intron", "NO_SITE", "ONE_SITE_INTERGENIC"),
   
   # reference genome and hgnc annotation
-  # ref_bsgenome = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
-  # ccds_filename_dir= "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
+  ref_bsgenome = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
+  ccds_filename_dir = "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
   
-  ref_bsgenome = "BSgenome.Hsapiens.UCSC.hg38",
-  ccds_filename_dir = "annotation_file/CCDS.20221027.txt",
-  hgnc_link= "https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt",
+  # ref_bsgenome = "BSgenome.Hsapiens.UCSC.hg38",
+  # ccds_filename_dir = "annotation_file/CCDS.20221027.txt",
   
+  hgnc_link = "https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt",
+   
   # alignment file location and criteria
-  # guide_aln_granges_genome = "T2T-CHM13v2.0",
-  guide_aln_granges_genome = "hg38",
-  aln_file_dir = "./object_intermediate/hg38/",
+  guide_aln_granges_genome = "T2T-CHM13v2.0",
+  # guide_aln_granges_genome = "hg38",
+  aln_file_dir = "./object_intermediate/T2T-CHM13_bsgenome/",
   
   # turn this on for T2T-CHM13
-  # check_index_file = "T2T-CHM13_BSgenome.1.ebwt",
-  # bowtie_index_dir = "bowtie_index/T2T-CHM13_bsgenome/",
-  # name_prefix_for_index = "T2T-CHM13_BSgenome",
+  check_index_file = "T2T-CHM13_BSgenome.1.ebwt",
+  bowtie_index_dir = "bowtie_index/T2T-CHM13_bsgenome/",
+  name_prefix_for_index = "T2T-CHM13_BSgenome",
   
   # turn this on for hg38
-  check_index_file = "hg38.1.ebwt",
-  bowtie_index_dir = "bowtie_index/hg38/",
-  name_prefix_for_index = "hg38",
+  # check_index_file = "hg38.1.ebwt",
+  # bowtie_index_dir = "bowtie_index/hg38/",
+  # name_prefix_for_index = "hg38",
   
   remove_all_single_mismatch = FALSE,
   
   # output dir
-  output_dir = "./output_cleaning/"
+  output_dir = "./output_cleaning/T2T-CHM13/"
 )
 
 
