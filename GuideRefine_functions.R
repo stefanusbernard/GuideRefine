f1e0fbbce6cc50a918d62d1bb2d51feeae38617c
Stefanus Bernard <79132254+stefanusbernard@users.noreply.github.com>
2026-05-07 09:13:58 +0100

    fix bugs

--- GuideRefine_functions.R
+++ GuideRefine_functions.R
@@ -1,27 +1,17 @@
 library(tidyverse)
-library(dplyr)
-library(stringr)
+
+STANDARD_CHROMOSOMES <- paste0("chr", c(1:22, "X", "Y"))
 
 #### MAIN FUNCTIONS
 
 # function_df_count
 function_df_count <- function(df_count, count_sgrna, num_genes, num_alignments, notes){
   df_count <- rbind(df_count, c(count_sgrna, num_genes, num_alignments, notes))
-  kable(df_count)
+  print(kable(df_count))
   return(df_count)
 }
 
 # FUNCTION TO REMOVE MULTI-TARGET-/OFF-TARGET SGRNAS
-
-# function to keep alignment only for normal chromosome
-alignment_normal_chr <- function(alignment_data) {
-    list_chromosome <- paste0('chr', c(1:22, 'X', 'Y'))
-    alignment_data <- alignment_data %>%
-        filter(chr %in% list_chromosome | is.na(chr))
-
-    return(alignment_data)    
-}
-
 
 # function to remove multi-target sgRNA
 multi_target_sgrna <- function(alignment_data){
@@ -50,7 +40,7 @@ multi_target_sgrna <- function(alignment_data){
         dplyr::rename('multi-target sgRNA' = 'n')
     
     # List of multi-target sgRNAs
-    multi_target_sgRNA_list_guides <- c(unique(multi_target_sgRNAs_count_dropped_guides_per_gene$sgRNA))
+    multi_target_sgRNA_list_guides <- unique(multi_target_sgRNAs_count_dropped_guides_per_gene$sgRNA)
 
     return(list(
         dropped_guides_num = multi_target_sgRNAs_count_dropped_guides_per_gene,
@@ -70,7 +60,7 @@ single_mismatch_sgrna <- function(alignment_data) {
         count(gene) %>%
         dplyr::rename('single mismatch sgRNA' = 'n')
 
-    single_mismatch_sgRNA_list_guides <- c(unique(single_mismatch_sgRNA_count_dropped_guides_per_gene$sgRNA))
+    single_mismatch_sgRNA_list_guides <- unique(single_mismatch_sgRNA_count_dropped_guides_per_gene$sgRNA)
 
     return(list(
         dropped_guides_num = single_mismatch_sgRNA_count_dropped_guides_per_gene,
@@ -81,38 +71,36 @@ single_mismatch_sgrna <- function(alignment_data) {
 }
 
 # function to remove sgRNA aligned with mismatches at the most PAM distal site
-pam_distal_mismatch <- function(alignment_data, mismatches){
+pam_distal_mismatch <- function(alignment_data){
   
-    annotate_cut_pos <- alignment_data %>% 
-        dplyr::rename(pam_start = pam_site) %>%
-        mutate(
-            pam_start = ifelse(strand == '+', pam_start, pam_start-2),
-            pam_end = ifelse(strand == '+', pam_start+2, pam_start+2),
-            start_pos = ifelse(strand == '+', pam_start-21, pam_start+3),
-            end_pos = ifelse(strand == '+', pam_start-1, pam_start+23),
-            cut_pos = ifelse(strand == '+', pam_start-4, pam_start+5)) %>%
-        relocate(pam_start, .after = cut_pos) %>%
-        relocate(start_pos, .before = cut_pos) %>%
-        relocate(end_pos, .after = start_pos) %>%
-        relocate(pam_end, .after = pam_start)
+    annotate_cut_pos <- annotate_pam_positions(alignment_data)
     
     
     # There is a possibility that a single guide share the pam-distal single mismatch and pam-distal double mismatch
     # that is OK as long as they are detected in either one condition
     
     detect_pam_distal_mismatches <- annotate_cut_pos %>%
-        rowwise() %>%
         mutate(
-            mismatch_loc = list(which(strsplit(spacer, '')[[1]] != strsplit(protospacer, '')[[1]])),
-            type = case_when(is.na(mismatch_loc[1]) | mismatch_loc[1] == 0 ~ "perfect",
-                             strand == "+" & mismatch_loc[1] < 3 & is.na(mismatch_loc[2]) ~ "pam-distal single mismatch",
-                             strand == "-" & mismatch_loc[1] > 18 & is.na(mismatch_loc[2]) ~ "pam-distal single mismatch",
-                             strand == '+' & (mismatch_loc[1] < 3 & mismatch_loc[2] < 3) ~ "pam-distal double mismatch",
-                             strand == '-' & (mismatch_loc[1] > 18 & mismatch_loc[2] > 18) ~ "pam-distal double mismatch",
-                             length(mismatch_loc) < 2 ~ "single mismatch",
-                             length(mismatch_loc) > 1 ~ "double mismatch",
-                             TRUE ~ "Uncategorized"
-            ))
+            mismatch_loc = mapply(
+                function(s, p) which(strsplit(s, "")[[1]] != strsplit(p, "")[[1]]),
+                spacer, protospacer,
+                SIMPLIFY = FALSE
+            ),
+            n_mm = lengths(mismatch_loc),
+            pos1 = vapply(mismatch_loc, function(x) if (length(x) >= 1) x[1L] else NA_integer_, integer(1L)),
+            pos2 = vapply(mismatch_loc, function(x) if (length(x) >= 2) x[2L] else NA_integer_, integer(1L)),
+            type = case_when(
+                n_mm == 0                              ~ "perfect",
+                strand == "+" & pos1 < 3 & n_mm == 1  ~ "pam-distal single mismatch",
+                strand == "-" & pos1 > 18 & n_mm == 1 ~ "pam-distal single mismatch",
+                strand == "+" & pos1 < 3 & pos2 < 3   ~ "pam-distal double mismatch",
+                strand == "-" & pos1 > 18 & pos2 > 18 ~ "pam-distal double mismatch",
+                n_mm == 1                              ~ "single mismatch",
+                n_mm > 1                               ~ "double mismatch",
+                TRUE                                   ~ "Uncategorized"
+            )
+        ) %>%
+        select(-n_mm, -pos1, -pos2)
     
     # WARNING: you pull sgRNA using the alignment data, dont forget to use unique() as the number of sgRNA will be duplicated instead
     
@@ -170,13 +158,11 @@ read_ccds_data <- function(ccds_filename){
     ccds$cds_from <- as.integer(ccds$cds_from)
     ccds$cds_to <- as.integer(ccds$cds_to)
 
-    list_chromosome <- paste0('chr', c(1:22, 'X', 'Y'))
-
     ccds <- ccds %>%
         dplyr::rename('chromosome' = '#chromosome') %>%
         mutate(chromosome = str_c("chr", chromosome)) %>%
         filter(ccds_status %in% c("Public", "Reviewed, update pending", "Under review, update")) %>%
-        filter(chromosome %in% list_chromosome, !is.na(cds_from), !is.na(cds_to))
+        filter(chromosome %in% STANDARD_CHROMOSOMES, !is.na(cds_from), !is.na(cds_to))
 
     return(ccds)
 
@@ -184,8 +170,7 @@ read_ccds_data <- function(ccds_filename){
 
 # function to transform gene annotation CCDS
 transform_gene_annotation_ccds <- function(imported_ccds_data, chosen_genome){
-    list_chromosome <- paste0('chr', c(1:22, 'X', 'Y'))
-    genome_info <- GenomeInfoDb::Seqinfo(genome = chosen_genome)[list_chromosome]
+    genome_info <- GenomeInfoDb::Seqinfo(genome = chosen_genome)[STANDARD_CHROMOSOMES]
     
     ccds_exon <- imported_ccds_data %>%
         mutate(cds_interval = str_replace_all(cds_locations, "[\\[\\]]", "") %>% str_split("\\s*,\\s*")) %>%
@@ -339,36 +324,40 @@ update_gene_symbol <- function(gene_df) {
   
   # join gene_df_mismatch with gene_df_match to find the duplicated sgRNA
   gene_df_mismatch <- gene_df_mismatch %>%
-    full_join(corrected_exist_gene) %>%
+    full_join(corrected_exist_gene, by = names(corrected_exist_gene)) %>%
     arrange(gene, desc(notes)) %>%
     mutate(duplicate_spacer_gene = duplicated(spacer, gene))
   
   # full_join the gene_df_mismatch with gene_df_match to become a gene_df_corrected
   gene_df_corrected <- gene_df_match %>%
-    full_join(gene_df_mismatch)
+    full_join(gene_df_mismatch, by = names(gene_df_mismatch))
   
   return(list("gene_symbol_check" = gene_df_check, 
               "gene_symbol_corrected" = gene_df_corrected))
+}
+
+annotate_pam_positions <- function(alignment_data) {
+    alignment_data %>%
+        dplyr::rename(pam_start = pam_site) %>%
+        mutate(
+            pam_start = ifelse(strand == '+', pam_start, pam_start-2),
+            pam_end   = ifelse(strand == '+', pam_start+2, pam_start+2),
+            start_pos = ifelse(strand == '+', pam_start-21, pam_start+3),
+            end_pos   = ifelse(strand == '+', pam_start-1, pam_start+23),
+            cut_pos   = ifelse(strand == '+', pam_start-4, pam_start+5)) %>%
+        relocate(pam_start, .after = cut_pos) %>%
+        relocate(start_pos, .before = cut_pos) %>%
+        relocate(end_pos, .after = start_pos) %>%
+        relocate(pam_end, .after = pam_start)
 }
 
 #### TEMPORARY FUNCTION
 
 # temporary function to meet the requirement of cut_pos for granges alignments
 add_cut_pos_pam_pos <- function(alignment_data){
-  alignment_data <- alignment_data %>%
-    dplyr::rename(pam_start = pam_site) %>%
-    mutate(
-      pam_start = ifelse(strand == '+', pam_start, pam_start-2),
-      pam_end = ifelse(strand == '+', pam_start+2, pam_start+2),
-      start_pos = ifelse(strand == '+', pam_start-21, pam_start+3),
-      end_pos = ifelse(strand == '+', pam_start-1, pam_start+23),
-      cut_pos = ifelse(strand == '+', pam_start-4, pam_start+5),
-      unique_aln_id = paste0('sgrna_aln_', row_number())) %>%
-    relocate(pam_start, .after = cut_pos) %>%
-    relocate(start_pos, .before = cut_pos) %>%
-    relocate(end_pos, .after = start_pos) %>%
-    relocate(pam_end, .after = pam_start) %>%
+  alignment_data <- annotate_pam_positions(alignment_data) %>%
+    mutate(unique_aln_id = paste0('sgrna_aln_', row_number())) %>%
     relocate(unique_aln_id, .before = sgRNA)
-  
+
   return(alignment_data)
 }
