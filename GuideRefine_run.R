5bc78a097fb289e6a159fc72b9937bf79ec9ba59
Stefanus Bernard <79132254+stefanusbernard@users.noreply.github.com>
2026-05-07 09:14:50 +0100

    fix fasta file missing path and minor bugs

--- GuideRefine_run.R
+++ GuideRefine_run.R
@@ -2,36 +2,38 @@ library(rmarkdown)
 
 # Define parameters
 my_params <- list(
-  output_filename = "tkov3_refined_report",
+  output_filename = "Jacquere_PerGuideAnnotations_Quota4",
   
   library_dir = "./public_crispr_library/",
-  sgrna_library = "tkov3_guide_sequence",
+  sgrna_library = "Jacquere_PerGuideAnnotations_Quota4",
   terms = c("CONTROL", "Control", "control", "INTRON", "Intron", "intron", "NO_SITE", "ONE_SITE_INTERGENIC", "LacZ", "luciferase", "EGFR"),
   
   # reference genome and hgnc annotation
-  ref_bsgenome = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
-  ccds_filename_dir = "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
+  # ref_bsgenome = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
+  # ccds_filename_dir = "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
   
-  # ref_bsgenome = "BSgenome.Hsapiens.UCSC.hg38",
-  # ccds_filename_dir = "annotation_file/CCDS.20221027.txt",
+  ref_bsgenome = "BSgenome.Hsapiens.UCSC.hg38",
+  ccds_filename_dir = "annotation_file/CCDS.20221027.txt",
   
   hgnc_link = "https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt",
    
   # alignment file location and criteria
-  guide_aln_granges_genome = "T2T-CHM13v2.0",
-  # guide_aln_granges_genome = "hg38",
-  aln_file_dir = "./object_intermediate/T2T-CHM13_bsgenome/",
+  # guide_aln_granges_genome = "T2T-CHM13v2.0",
+  guide_aln_granges_genome = "hg38",
+  aln_file_dir = "./object_intermediate/hg38/",
   
   # turn this on for T2T-CHM13
-  check_index_file = "T2T-CHM13_BSgenome.1.ebwt",
-  bowtie_index_dir = "bowtie_index/T2T-CHM13_bsgenome/",
-  name_prefix_for_index = "T2T-CHM13_BSgenome",
+  # check_index_file = "T2T-CHM13_BSgenome.1.ebwt",
+  # bowtie_index_dir = "bowtie_index/T2T-CHM13_bsgenome/",
+  # name_prefix_for_index = "T2T-CHM13_BSgenome",
   
   # turn this on for hg38
-  # check_index_file = "hg38.1.ebwt",
-  # bowtie_index_dir = "bowtie_index/hg38/",
-  # name_prefix_for_index = "hg38",
-  
+  check_index_file = "hg38.1.ebwt",
+  bowtie_index_dir = "bowtie_index/hg38/",
+  name_prefix_for_index = "hg38",
+  fasta_file = "",  # set path to reference FASTA if bowtie index needs to be built, e.g. "reference/hg38.fa"
+
+  # Default parameter is to remove all sgRNA aligned with single mismatch at any 20 nucleotides spacer
   remove_all_single_mismatch = TRUE,
   
   # output dir
@@ -51,10 +53,9 @@ output_file <- paste0(my_params$output_dir, my_params$output_filename, "_", date
 # Render pipeline
 rmarkdown::render(
   input = "GuideRefine.Rmd",
-  output_file = paste0(my_params$output_dir, my_params$output_filename, ".html"),
+  output_file = output_file,
   params = my_params,
   envir = new.env()
 )
 
-
-# message("✅ Pipeline rendered successfully: ", file.path(my_params$output_dir, output_file))
+message("Pipeline rendered successfully: ", output_file)
