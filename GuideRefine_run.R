254e91ee0faaba871a72f423ff084d699f7eea67
Stefanus Bernard <79132254+stefanusbernard@users.noreply.github.com>
2026-05-09 13:06:13 +0100

    only use T2T-chm13 parameters

--- GuideRefine_run.R
+++ GuideRefine_run.R
@@ -1,43 +1,39 @@
 library(rmarkdown)
 
+if (!rmarkdown::pandoc_available()) {
+  stop(
+    "Pandoc >= 1.12.3 is required but was not found.\n",
+    "Run GuideRefine_install_req_packages.R to install it automatically."
+  )
+}
+
 # Define parameters
 my_params <- list(
-  output_filename = "Jacquere_PerGuideAnnotations_Quota4",
+  output_filename = "avana_library",
   
   library_dir = "./public_crispr_library/",
-  sgrna_library = "Jacquere_PerGuideAnnotations_Quota4",
+  sgrna_library = "avana_library",
   terms = c("CONTROL", "Control", "control", "INTRON", "Intron", "intron", "NO_SITE", "ONE_SITE_INTERGENIC", "LacZ", "luciferase", "EGFR"),
   
   # reference genome and hgnc annotation
-  # ref_bsgenome = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
-  # ccds_filename_dir = "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
-  
-  ref_bsgenome = "BSgenome.Hsapiens.UCSC.hg38",
-  ccds_filename_dir = "annotation_file/CCDS.20221027.txt",
-  
+  ref_bsgenome = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
+  ccds_filename_dir = "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
   hgnc_link = "https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt",
    
   # alignment file location and criteria
-  # guide_aln_granges_genome = "T2T-CHM13v2.0",
-  guide_aln_granges_genome = "hg38",
-  aln_file_dir = "./object_intermediate/hg38/",
+  guide_aln_granges_genome = "T2T-CHM13v2.0",
+  aln_file_dir = "./object_intermediate/May2026_T2T-CHM13_bsgenome/",
   
   # turn this on for T2T-CHM13
-  # check_index_file = "T2T-CHM13_BSgenome.1.ebwt",
-  # bowtie_index_dir = "bowtie_index/T2T-CHM13_bsgenome/",
-  # name_prefix_for_index = "T2T-CHM13_BSgenome",
+  check_index_file = "T2T-CHM13_BSgenome.1.ebwt",
+  bowtie_index_dir = "bowtie_index/T2T-CHM13_bsgenome/",
+  name_prefix_for_index = "T2T-CHM13_BSgenome",
   
-  # turn this on for hg38
-  check_index_file = "hg38.1.ebwt",
-  bowtie_index_dir = "bowtie_index/hg38/",
-  name_prefix_for_index = "hg38",
-  fasta_file = "",  # set path to reference FASTA if bowtie index needs to be built, e.g. "reference/hg38.fa"
-
   # Default parameter is to remove all sgRNA aligned with single mismatch at any 20 nucleotides spacer
   remove_all_single_mismatch = TRUE,
   
   # output dir
-  output_dir = "./output_cleaning/"
+  output_dir = "./output_cleaning/May2026_T2T-CHM13/"
 )
 
 
