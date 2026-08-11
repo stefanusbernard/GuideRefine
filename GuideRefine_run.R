a2f9eed77ca237067c5d75c6217742a8d117b957
Stefanus Bernard <stefanus.bernard@gmail.com>
2026-08-11 12:31:02 +0100

    change the script input as a template

--- GuideRefine_run.R
+++ GuideRefine_run.R
@@ -7,46 +7,55 @@ if (!rmarkdown::pandoc_available()) {
   )
 }
 
-# Define parameters
 my_params <- list(
-  output_filename = "avana_library",
-  
-  library_dir = "./public_crispr_library/",
-  sgrna_library = "avana_library",
-  terms = c("CONTROL", "Control", "control", "INTRON", "Intron", "intron", "NO_SITE", "ONE_SITE_INTERGENIC", "LacZ", "luciferase", "EGFR"),
-  
-  # reference genome and hgnc annotation
-  ref_bsgenome = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
-  ccds_filename_dir = "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
-  hgnc_link = "https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt",
-   
-  # alignment file location and criteria
-  guide_aln_granges_genome = "T2T-CHM13v2.0",
-  aln_file_dir = "./object_intermediate/May2026_T2T-CHM13_bsgenome/",
-  
-  # turn this on for T2T-CHM13
-  check_index_file = "T2T-CHM13_BSgenome.1.ebwt",
-  bowtie_index_dir = "bowtie_index/T2T-CHM13_bsgenome/",
-  name_prefix_for_index = "T2T-CHM13_BSgenome",
-  
-  # Default parameter is to remove all sgRNA aligned with single mismatch at any 20 nucleotides spacer
+  output_filename = "my_library",
+  library_dir     = "./public_crispr_library/processed/",
+  sgrna_library   = "my_library",
+  terms = c("CONTROL", "Control", "control", "INTRON", "NO_SITE"),
+
+  ref_bsgenome             = "BSgenome.Hsapiens.UCSC.hg38",
+  ccds_filename_dir        = "annotation_file/CCDS.20221027.txt",
+  hgnc_link                = "https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt",
+  guide_aln_granges_genome = "hg38",
+  aln_file_dir             = "./object_intermediate/hg38/",
+  check_index_file         = "hg38.1.ebwt",
+  bowtie_index_dir         = "bowtie_index/hg38/",
+  name_prefix_for_index    = "hg38",
+  fasta_file               = "",
+
   remove_all_single_mismatch = TRUE,
-  
-  # output dir
-  output_dir = "./output_cleaning/May2026_T2T-CHM13/"
+  output_dir = "./output_cleaning/"
 )
+
+# T2T-CHM13: build the BSgenome package and CCDS annotation first (see
+# annotation_file/README.md), then swap in this block instead.
+#
+# my_params <- list(
+#   output_filename = "my_library",
+#   library_dir     = "./public_crispr_library/processed/",
+#   sgrna_library   = "my_library",
+#   terms = c("CONTROL", "Control", "control", "INTRON", "NO_SITE"),
+#   ref_bsgenome             = "BSgenome.Hsapiens.NCBI.T2TCHM13v2.0",
+#   ccds_filename_dir        = "annotation_file/T2T-CHM13v2.0_gene_annot_granges.rds",
+#   hgnc_link                = "https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt",
+#   guide_aln_granges_genome = "T2T-CHM13v2.0",
+#   aln_file_dir             = "./object_intermediate/T2T-CHM13_bsgenome/",
+#   check_index_file         = "T2T-CHM13_BSgenome.1.ebwt",
+#   bowtie_index_dir         = "bowtie_index/T2T-CHM13_bsgenome/",
+#   name_prefix_for_index    = "T2T-CHM13_BSgenome",
+#   fasta_file               = "",
+#   remove_all_single_mismatch = TRUE,
+#   output_dir = "./output_cleaning/"
+# )
 
 
 if (!dir.exists(my_params$output_dir)) {
   dir.create(my_params$output_dir, recursive = TRUE)
 }
 
-
 date_tag <- format(Sys.Date(), "%Y%m%d")
 output_file <- paste0(my_params$output_dir, my_params$output_filename, "_", date_tag, ".html")
 
-
-# Render pipeline
 rmarkdown::render(
   input = "GuideRefine.Rmd",
   output_file = output_file,
